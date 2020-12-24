a69e0a1c93ba04e5d7b9c9979a7a5e07ae509bdb
Mike Skec <skec@protonmail.ch>
2020-12-24 22:11:32 +1100

    Fix GitHub theme

--- github-dark.js
+++ github-dark.js
@@ -2,3 +2,4 @@
  * Simply set <html> tag's 'data-color-mode' attribute to 'dark'
  */
 document.documentElement.setAttribute("data-color-mode", "dark");
+document.querySelector('.js-details-container').classList.remove("Header-old");
